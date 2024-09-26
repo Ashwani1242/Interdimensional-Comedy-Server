3075cc4034f540f88147fc9f5211820fb093ce6a
Ashwani1242 <ashwanigupta1242@gmail.com>
2024-09-26 19:53:01 +0530

    Changes Committed on 2024-09-26, 19:53

--- controllers/musicgen.controller.js
+++ controllers/musicgen.controller.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 export const generateMusic = async (req, res) => {
     const { is_auto, title, prompt, isInstrumental } = req.body;
 
-    // Log incoming request data
     console.log("Received request to generate music:", {
         is_auto,
         title,
@@ -11,7 +10,6 @@ export const generateMusic = async (req, res) => {
         isInstrumental,
     });
 
-    // Prepare the request data for the external API
     const requestData = {
         is_auto,
         prompt,
@@ -21,7 +19,6 @@ export const generateMusic = async (req, res) => {
     };
 
     try {
-        // Log the request data being sent to the external API
         console.log("Sending request to music generation API with data:", requestData);
 
         const response = await axios.post('https://api.topmediai.com/v1/music', requestData, {
@@ -31,7 +28,6 @@ export const generateMusic = async (req, res) => {
             },
         });
 
-        // Log the response from the music generation API
         console.log("Received response from music generation API:", response.data);
 
         const musicData = response.data;
@@ -41,10 +37,8 @@ export const generateMusic = async (req, res) => {
             music: musicData,
         });
     } catch (error) {
-        // Log the error if the request fails
         console.error('Error generating music:', error);
 
-        // Check if error has a response object
         if (error.response) {
             console.error('API Response Error:', error.response.data);
             return res.status(error.response.status || 500).json({
