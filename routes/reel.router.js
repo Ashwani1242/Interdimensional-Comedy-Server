40fd3c96fa64ffc9deccde8e1fab2760fe8750a3
Ashwani1242 <ashwanigupta1242@gmail.com>
2024-10-07 12:14:36 +0530

    Changes Committed on 2024-10-07, 12:14

--- routes/reel.router.js
+++ routes/reel.router.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-const reelGenRoute = Router();
 import multer from 'multer';
+
+const reelGenRoute = Router();
 import { generateSummaryController } from '../controllers/reel.controller.js';
 
 const upload = multer({ dest: 'uploads/' });
